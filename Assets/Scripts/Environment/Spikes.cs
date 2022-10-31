4e4901069c36d471e28d8e784681615b3d056322
E1win <87700553+E1win@users.noreply.github.com>
2022-10-31 11:49:34 +0100

    v2.2.0. Pause Menu, Spikes, Game Complete
    - Added text in Level 1 to teach player about pause menu
    - Changed Spikes from OnTriggerEnter2D to OnTriggerStay2D.
    - Created Game Complete Panel
    - Added Life-Up Heart to Level 2.

--- Assets/Scripts/Environment/Spikes.cs
+++ Assets/Scripts/Environment/Spikes.cs
@@ -4,9 +4,28 @@ using UnityEngine;
 
 public class Spikes : MonoBehaviour
 {
+    // void OnTriggerEnter2D(Collider2D other) {
+    //     if (other.gameObject.tag == "Player") {
+    //         PlayerManager player = other.gameObject.GetComponent<PlayerManager>();
+    //         if (!player.IsInvinsible()) {
+    //             player.Kill();
+    //         }
+    //     }
+    // }
+
+    // add void start and save player first time trigger is entered.
+
+    private PlayerManager player;
+
     void OnTriggerEnter2D(Collider2D other) {
         if (other.gameObject.tag == "Player") {
-            PlayerManager player = other.gameObject.GetComponent<PlayerManager>();
+            player = other.gameObject.GetComponent<PlayerManager>();
+        }
+    }
+
+    private void OnTriggerStay2D(Collider2D other) {
+        if (other.gameObject.tag == "Player") {
+            //PlayerManager player = other.gameObject.GetComponent<PlayerManager>();
             if (!player.IsInvinsible()) {
                 player.Kill();
             }
