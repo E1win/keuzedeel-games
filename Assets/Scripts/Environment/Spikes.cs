781a8ecda3537f7fa67c7a461004b3895eabfbc9
E1win <87700553+E1win@users.noreply.github.com>
2022-11-03 15:33:38 +0100

    v1.6.0. Enemy Animations, Player Movement
    - Added animations to enemy movement
    - Minor tweaks to player movement.

--- Assets/Scripts/Environment/Spikes.cs
+++ Assets/Scripts/Environment/Spikes.cs
@@ -4,15 +4,6 @@ using UnityEngine;
 
 public class Spikes : MonoBehaviour
 {
-    // void OnTriggerEnter2D(Collider2D other) {
-    //     if (other.gameObject.tag == "Player") {
-    //         PlayerManager player = other.gameObject.GetComponent<PlayerManager>();
-    //         if (!player.IsInvinsible()) {
-    //             player.Kill();
-    //         }
-    //     }
-    // }
-
     private PlayerManager player;
 
     void OnTriggerEnter2D(Collider2D other) {
